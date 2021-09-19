f94837d82a2e540da354b690d937b452854dd30f
Vineet Ajmani <83583852+VineetAjmani01@users.noreply.github.com>
2021-09-19 16:45:46 +0530

    Add files via upload

---
+++ LiveToCode/courses.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+
+
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  &nbsp;   &nbsp;   &nbsp;   &nbsp; <a class="navbar-brand" href="#">LiveToCode</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#courses.php">Free Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pro.php">Paid Courses</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+  <div class="jumbotron">
+  <h1>Free Courses</h1>
+  <p>Learn In-Demand Career Skills From Experts In Your Chosen Field. <br>
+   No Relevant Experience Or Degree Is Required To Enroll. <br>
+   Get World-Class Learning On-Demand.</p>
+</div>
+
+
+<section class="my-5">
+
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <img src="img/3.jpg" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <h2 class="display-4">Java Tutorials (Free)</h2>
+        <p class="py-3">In this complete Core + Advance Java tutorial for beginners in hindi, we will see complete java topics covered in hindi. This java complete tutorial will cover topics like Downloading Java, Installation of Java, java JVM, java inheritance, java inheritance & polymorphism and other complete java tutorial topics will be covered.
+This Java Tutorial for Beginners in Hindi can be used to learn Java in Hindi. Hope you like this Java Tutorial in Hindi. I am very excited to launch this Java full course for you!
+          </p>
+          <a href="https://youtube.com/playlist?list=PLu0W_9lII9agS67Uits0UnJyrYiXhDS6q" class="btn btn-success">Enroll Now</a>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="my-5">
+
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <img src="img/3.jpg" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <h2 class="display-4">Data Structures and Algorithms (Free)</h2>
+        <p class="py-3"> This Algorithms and Data Structures course will teach you everything you need to prepare for placements, interviews and logic building. 
+This playlist will teach you how to create optimal solutions to your real world problems. We will cover a wide variety of data structures and algorithms in this course. Make sure to access the notes I have provided along with the course.
+          </p>
+          <a href="https://youtube.com/playlist?list=PLu0W_9lII9ahIappRPN0MCAgtOu3lQjQi" class="btn btn-success">Enroll Now</a>
+      </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <img src="img/3.jpg" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <h2 class="display-4">C Programming (Free)</h2>
+        <p class="py-3"> In this latest course on C language tutorials in 2019 in Hindi , we will learn how to write efficient and powerful C programs using modern tools. 
+C programming is one of the most requested topics on this channel which is why I created this c programming for beginners course. Learning C programming in Hindi is a good decision for people who are comfortable in learning in their mother tongue rather than English. 
+          </p>
+          <a href="https://youtube.com/playlist?list=PLu0W_9lII9aiXlHcLx-mDH1Qul38wD3aR" class="btn btn-success">Enroll Now</a>
+      </div>
+    </div>
+  </section>
+
+
+  
+<footer>
+  <p class="p-3 bg-dark text-white text-center">Contact Us - livetocode69@gmail.com
+  </p>
+</footer>
+
+
+
+</body>
+</html>
---
+++ LiveToCode/pro.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+
+
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  &nbsp;   &nbsp;   &nbsp;   &nbsp; <a class="navbar-brand" href="#">LiveToCode</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="courses.php">Free Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pro.php">Paid courses</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+  <div class="jumbotron">
+  <h1>Paid Courses</h1>
+  <p> Learn In-Demand Career Skills From Experts In Your Chosen Field. <br>
+   No Relevant Experience Or Degree Is Required To Enroll. <br>
+   Get World-Class Learning On-Demand.</p>
+</div>
+
+
+<section class="my-5">
+
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <img src="img/3.jpg" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <h2 class="display-4">PHP for Beginners</h2>
+        <p class="py-3"> Are you wanting to get into Web development, but are not sure where to start? 
+
+So maybe you tried out some PHP and MySQL tutorials on Youtube, but found them too hard or outdated, and feel that you need a more comprehensive introduction.
+
+Maybe you want to learn how to write PHP the right way.  Secure code, OOP ready, and maximising re-usability as well as finding out about PHP programming standards to adopt and adhere too?
+          </p>
+          <a href="https://www.udemy.com/course/php-for-beginners-/" class="btn btn-success">Enroll Now</a>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="my-5">
+
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <img src="img/3.jpg" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <h2 class="display-4">Angular - The Complete Guide</h2>
+        <p class="py-3"> Join the most comprehensive, popular and bestselling Angular course on Udemy and benefit not just from a proven course concept but from a huge community as well! 
+
+From Setup to Deployment, this course covers it all! You'll learn all about Components, Directives, Services, Forms, Http Access, Authentication, Optimizing an Angular App with Modules and Offline Compilation and much more - and in the end: You'll learn how to deploy an application!
+          </p>
+          <a href="https://www.udemy.com/course/the-complete-guide-to-angular-2/?src=sac&kw=" class="btn btn-success">Enroll Now</a>
+      </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <img src="img/3.jpg" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12"> 
+        <h2 class="display-4">Python For Absolute Beginners</h2>
+        <p class="py-3"> Learn Python, a programming language with a wide variety of well-paying jobs in many fields, including data science and web development. 
+
+It is relatively easy to learn when compared to pretty much any other programming language since most of the time, using Python to do something will take less lines of code than with another programming language.
+
+In this course, you will be taught the fundamentals of Python through video lectures, quizzes, review exercises, and programming challenges.
+          </p>
+          <a href="https://www.udemy.com/course/python-for-absolute-beginners-u/" class="btn btn-success">Enroll Now</a>
+      </div>
+    </div>
+  </section>
+  
+<footer>
+  <p class="p-3 bg-dark text-white text-center">Contact Us - livetocode69@gmail.com
+  </p>
+</footer>
+
+
+
+</body>
+</html>
